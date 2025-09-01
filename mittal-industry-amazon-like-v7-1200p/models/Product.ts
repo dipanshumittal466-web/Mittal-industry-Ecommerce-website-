37213d445d45b3614c864c08b373edf1700731cc
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-09-01 13:18:24 +0530

    Update Product.ts

--- mittal-industry-amazon-like-v7-1200p/models/Product.ts
+++ mittal-industry-amazon-like-v7-1200p/models/Product.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+
 const ProductSchema = new mongoose.Schema({
   name: String,
   brand: String,
@@ -10,4 +11,5 @@ const ProductSchema = new mongoose.Schema({
   rating: { type: Number, default: 4.3 },
   reviews: { type: Number, default: 100 }
 }, { timestamps: true });
+
 export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
