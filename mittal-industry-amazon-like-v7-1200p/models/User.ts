1987308fa10b0b67c749a61f440530aaba8526d4
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-09-01 12:57:23 +0530

    Update User.ts

--- mittal-industry-amazon-like-v7-1200p/models/User.ts
+++ mittal-industry-amazon-like-v7-1200p/models/User.ts
@@ -1,8 +1,23 @@
-import mongoose, { Schema, model, models } from "mongoose";
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: { type: String, default: "admin" }
-}, { timestamps: true });
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: { type: String, default: "admin" },
+  },
+  { timestamps: true }
+);
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+
+export default User;
