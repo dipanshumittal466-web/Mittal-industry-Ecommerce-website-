669215601cb3c5d2e4f3a91655422a0c8a65727d
dipanshumittal466-web <dipanshumittal466@gmail.com>
2025-09-01 15:32:45 +0530

    Create test.ts

---
+++ mittal-industry-amazon-like-v7-1200p/pages/api/test.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../lib/dbConnect";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await dbConnect();
+    res.status(200).json({ message: "MongoDB connected 🚀" });
+  } catch (error) {
+    res.status(500).json({ error: "Database connection failed ❌" });
+  }
+}
